b970db96652d416b748a451598cda3011bec7eda
Karim Toulba <karim@wadipurple.com>
2024-08-12 23:56:10 +0300

    Update index.php

--- gutenberg-quiz/index.php
+++ gutenberg-quiz/index.php
@@ -1,1 +1,36 @@
+<?php 
+
+/*
+Plugin Name: Attention Quiz
+Description: A simple plugin that will register a custom quiz block to the Gutenberg editor.
+Version: 1.0.0
+Author: Karim Toulba
+Author URI: https://karimtoulba.com
+Text Domain: quizdomain
+Domain Path: /languages
+*/
+
+if ( ! defined('ABSPATH') ) exit; // Exit if accessed directly
+
+class NewAttentionQuiz {
+
+    function __construct() {
+        add_action('init', array($this, 'toRegisterBlock')); 
+    }
+
+    function toRegisterBlock() {
+        wp_register_script( 'myNewPlugin', plugin_dir_url( __FILE__ ) . 'build/index.js', array('wp-blocks', 'wp-element') );
+        register_block_type( 'ourplugin/attentionquiz', array(
+            'editor_script' => 'myNewPlugin',
+            'render_callback' => array($this, 'myHTML')
+        ) );
+    }
+
+    function myHTML($attributes) {
+        return '<p>' . 'The sky is ' . $attributes['skyColor'] . ' and the grass is ' . $attributes['grassColor'] . '</p>' ;
+    }
+
+}
+
+$NewAttentionQuiz = new NewAttentionQuiz();
 
