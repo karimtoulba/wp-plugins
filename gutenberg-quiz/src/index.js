ddd3e5bb1658cb34b23a6264ee938d8b2d6f4302
Karim Toulba <karim@wadipurple.com>
2024-08-12 23:57:22 +0300

    Update index.js

--- gutenberg-quiz/src/index.js
+++ gutenberg-quiz/src/index.js
@@ -1,1 +1,44 @@
+wp.blocks.registerBlockType("ourplugin/attentionquiz", {
+  // Information
+  title: "Attention Quiz",
+  description: "A simple plugin to add attention quiz in posts.",
+  icon: "smiley",
+  category: "common",
+
+  // attributes
+  attributes: {
+    skyColor: {
+      type: "string",
+    },
+    grassColor: {
+      type: "string",
+    },
+  },
+
+  // Edit (on Backend)
+  edit: function (props) {
+    function theskyColor(event) {
+      props.setAttributes({ skyColor: event.target.value });
+    }
+    function thegrassColor(event) {
+      props.setAttributes({ grassColor: event.target.value });
+    }
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Sky Color"
+          value={props.attributes.skyColor}
+          onChange={theskyColor}
+        />
+        <input
+          type="text"
+          placeholder="Grass Color"
+          value={props.attributes.grassColor}
+          onChange={thegrassColor}
+        />
+      </div>
+    );
+  },
+});
 
